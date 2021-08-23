fec9201dc37cf0278ca815be77bbb36b0d9591f7
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-23 09:32:16 +0900

    JS: Scroll to

--- main2.js
+++ main2.js
@@ -2,11 +2,23 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(`navbarHeight: ${navbarHeight}`);
+  // console.log(window.scrollY);
+  // console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   };
+});
+
+const navberMenu = document.querySelector('.navbar__menu');
+navberMenu.addEventListener('click', () => {
+  const link = event.target.dataset.link;
+  if(link === null) {
+    return ;
+  } else {
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+  };
+  
 });
